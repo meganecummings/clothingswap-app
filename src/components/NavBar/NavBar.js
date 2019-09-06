b1863799cae6c5df02e0a44b3b44f08328f6d5a4
meganecummings <megcummings@gmail.com>
2019-09-06 11:40:00 -0700

    NavBar Init Complete

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,10 +2,45 @@ import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import './NavBar.css';
 
-const NavBar = ({  }) => {
+const NavBar = ({ logout, currentUser }) => {
 
+    const links = (
+        <div className="nav-items">
+            <li>
+                <NavLink className="nav-li" to="/login">Log In</NavLink>
+            </li>
+            <li>
+                <NavLink className="nav-li" to="/register">Register</NavLink>
+            </li>
+        </div>
+    );
 
-    
+    const authLinks = (
+        <div className="nav-items">
+            <li>
+                <NavLink className="nav-li" to="/profile">Profile</NavLink>
+            </li>
+            <li>
+                <NavLink className="nav-li" to="/events">Events</NavLink>
+            </li>
+            <li>
+                <NavLink className="nav-li" to="/items">Items</NavLink>
+            </li>
+            <li>
+                <span className="nav-li" onClick={logout} style={{ cursor: 'pointer' }}>Logout</span>
+            </li>
+        </div>
+    );
+
+    return (
+        <nav>
+            <div className="logo">
+                <Link className="nav-li" to="/"><h1>haute swap</h1></Link>
+            </div>
+            { currentUser ? authLinks : links }
+        </nav>
+    )
+
 };
 
 export default NavBar;
