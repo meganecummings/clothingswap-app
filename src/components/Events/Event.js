46932fa67fe5ada0533ffdecd9633bedcff51f17
meganecummings <megcummings@gmail.com>
2019-09-08 11:33:32 -0700

    EventPage Logic Update

--- src/components/Events/Event.js
+++ src/components/Events/Event.js
@@ -5,30 +5,24 @@ import Post from '../Post/Post';
 import './Event.css'
 import PostsContainer from '../../containers/PostsContainer/PostsContainer';
 
-const Event = ({ title, description, location, date, startTime, posts, invitees, slug, items, attendees, image }) => {
+const Event = ({ event: { _id, title, description, location, date, startTime, posts, invitees, attendees, image}, displayPosts }) => {
 
- 
-    const postsArr = posts.map(post => {
-        return(
-            <Link key={post._id} to={`/posts/${post._id}`} >
-                <Post key={post._id} title={post.title} authorUsername={post.authorUsername} description={post.description} content={post.content} />
-            </Link>
-        )
-    });
+    const eventDate = date && `${new Date(date).toLocaleDateString()} @ ${new Date(date).toLocaleTimeString()}`;
+
 
     return (
-        <div className="Event-card">
+        <div className="Event-card" key={_id}>
             <div >
                 <p><strong>{title}</strong></p>
             </div>
             <img src={image} alt={`${title}`} />
             <p>Where: {location}</p>
-            <p>When: {date}</p>
+            <p>When: {eventDate}</p>
             <p>Start Time: {startTime}</p>
             <p>What it's all about: {description}</p>
-            <p>Who's confirmed: {attendees}</p>
             <p>Who's Invited: {invitees}</p>
-            {posts.length && <PostsContainer posts={postsArr} />}
+            <p><Link to={`/events/${_id}`}> More Event Details </Link></p>
+            <p>{posts.length && <PostsContainer posts={displayPosts(posts)} />}</p>
         </div>
     )
 }
